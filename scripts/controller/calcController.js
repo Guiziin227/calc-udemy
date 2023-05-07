a01b5bbde44315e6d06f0cfc5c9b0ca9c5bbaacb
Guiziin227 <gh63388@gmail.com>
2023-05-07 11:38:44 -0300

    puxando html para o js

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -10,6 +10,13 @@ class CalcController {
 
     initialize(){
 
+        let displayCalcEl = document.querySelector('#display')
+        let horaEl = document.querySelector('#hora')
+        let dataEl = document.querySelector('#data')
+
+        displayCalcEl.innerHTML = '00'
+        horaEl.innerHTML = '21:28'
+        dataEl.innerHTML = '06/05/2023'
     }
 
      get displayCalc(){
